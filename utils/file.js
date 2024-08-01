9a173c05644c0c243afce3b21bf7f54463761977
rashaaz <rashaabdelaziz111@gmail.com>
2024-08-01 04:36:00 -0700

    add

---
+++ utils/file.js
@@ -0,0 +1,268 @@
+import { ObjectId } from 'mongodb';
+import { v4 as uuidv4 } from 'uuid';
+import { promises as fsPromises } from 'fs';
+import dbClient from './db';
+import userUtils from './user';
+import basicUtils from './basic';
+
+/**
+ * Module with file utilities
+ */
+const fileUtils = {
+  /**
+   * Validates the request body for file operations
+   * @request {request_object} The request object containing
+   * @return {object} An object containing error
+   */
+  async validateBody(request) {
+    const {
+      name, type, isPublic = false, data,
+    } = request.body;
+
+    let { parentId = 0 } = request.body;
+
+    const typesAllowed = ['file', 'image', 'folder'];
+    let msg = null;
+
+    if (parentId === '0') parentId = 0;
+
+    if (!name) {
+      msg = 'Missing name';
+    } else if (!type || !typesAllowed.includes(type)) {
+      msg = 'Missing type';
+    } else if (!data && type !== 'folder') {
+      msg = 'Missing data';
+    } else if (parentId && parentId !== '0') {
+      let file;
+
+      if (basicUtils.isValidId(parentId)) {
+        file = await this.getFile({
+          _id: ObjectId(parentId),
+        });
+      } else {
+        file = null;
+      }
+
+      if (!file) {
+        msg = 'Parent not found';
+      } else if (file.type !== 'folder') {
+        msg = 'Parent is not a folder';
+      }
+    }
+
+    const obj = {
+      error: msg,
+      fileParams: {
+        name,
+        type,
+        parentId,
+        isPublic,
+        data,
+      },
+    };
+
+    return obj;
+  },
+
+  /**
+   * Retrieves a file from the database
+   * @query {obj} The query to find
+   * @return {object} The file document
+   */
+  async getFile(query) {
+    const file = await dbClient.filesCollection.findOne(query);
+    return file;
+  },
+
+  /**
+   * Gets a list of file documents
+   * The query used to find files
+   * @query {obj} The query used to find files
+   * @return {Array} The list of files
+   */
+  async getFilesOfParentId(query) {
+    const fileList = await dbClient.filesCollection.aggregate(query);
+    return fileList;
+  },
+
+  /**
+   * Saves a file to the database and file system.
+   * @userId {string} The ID of the user saving the file
+   * @fileParams {obj} The parameters of the file to save.
+   * @FOLDER_PATH {string} The path to save the file.
+   * @return {obj} The result object containing
+   */
+  async saveFile(userId, fileParams, FOLDER_PATH) {
+    const {
+      name, type, isPublic, data,
+    } = fileParams;
+    let { parentId } = fileParams;
+
+    if (parentId !== 0) parentId = ObjectId(parentId);
+
+    const query = {
+      userId: ObjectId(userId),
+      name,
+      type,
+      isPublic,
+      parentId,
+    };
+
+    if (fileParams.type !== 'folder') {
+      const fileNameUUID = uuidv4();
+
+      // const fileDataDecoded = Buffer.from(data, 'base64').toString('utf-8');
+      const fileDataDecoded = Buffer.from(data, 'base64');
+
+      const path = `${FOLDER_PATH}/${fileNameUUID}`;
+
+      query.localPath = path;
+
+      try {
+        await fsPromises.mkdir(FOLDER_PATH, { recursive: true });
+        await fsPromises.writeFile(path, fileDataDecoded);
+      } catch (err) {
+        return { error: err.message, code: 400 };
+      }
+    }
+
+    const result = await dbClient.filesCollection.insertOne(query);
+
+    // query.userId = query.userId.toString();
+    // query.parentId = query.parentId.toString();
+
+    const file = this.processFile(query);
+
+    const newFile = { id: result.insertedId, ...file };
+
+    return { error: null, newFile };
+  },
+
+  /**
+   * Updates a file in the database.
+   * @query {obj} The query to find the file to update
+   * @set {obj} The update operations to apply.
+   * @return {object} The updated file document
+   */
+  async updateFile(query, set) {
+    const fileList = await dbClient.filesCollection.findOneAndUpdate(
+      query,
+      set,
+      { returnOriginal: false },
+    );
+    return fileList;
+  },
+
+  /**
+   * Publishes or unpublishes a file
+   * @request {request_object} The request object
+   * @setPublish {boolean} The flag to set the file
+   * @return {object} The result object containing
+   */
+  async publishUnpublish(request, setPublish) {
+    const { id: fileId } = request.params;
+
+    if (!basicUtils.isValidId(fileId)) { return { error: 'Unauthorized', code: 401 }; }
+
+    const { userId } = await userUtils.getUserIdAndKey(request);
+
+    if (!basicUtils.isValidId(userId)) { return { error: 'Unauthorized', code: 401 }; }
+
+    const user = await userUtils.getUser({
+      _id: ObjectId(userId),
+    });
+
+    if (!user) return { error: 'Unauthorized', code: 401 };
+
+    const file = await this.getFile({
+      _id: ObjectId(fileId),
+      userId: ObjectId(userId),
+    });
+
+    if (!file) return { error: 'Not found', code: 404 };
+
+    const result = await this.updateFile(
+      {
+        _id: ObjectId(fileId),
+        userId: ObjectId(userId),
+      },
+      { $set: { isPublic: setPublish } },
+    );
+
+    const {
+      _id: id,
+      userId: resultUserId,
+      name,
+      type,
+      isPublic,
+      parentId,
+    } = result.value;
+
+    const updatedFile = {
+      id,
+      userId: resultUserId,
+      name,
+      type,
+      isPublic,
+      parentId,
+    };
+
+    return { error: null, code: 200, updatedFile };
+  },
+
+  /**
+   * Processes a file document to prepare it for response.
+   * @doc {object} The file document.
+   * @return {object} The processed file object
+   */
+  processFile(doc) {
+    // Changes _id for id and removes localPath
+
+    const file = { id: doc._id, ...doc };
+
+    delete file.localPath;
+    delete file._id;
+
+    return file;
+  },
+
+  /**
+   * Checks if a user is the owner of a file
+   * spec us
+   * @file {object} The file document.
+   * @userId {string} The ID of the user
+   * @return {boolean} True if the user is the owner
+   */
+  isOwnerAndPublic(file, userId) {
+    if (
+      (!file.isPublic && !userId)
+      || (userId && file.userId.toString() !== userId && !file.isPublic)
+    ) { return false; }
+
+    return true;
+  },
+
+  /**
+   * Retrieves the file data from the file system
+   * @file {object} The file document
+   * @size {string} The size variant of the file
+   * @return {object} The result object containing
+   */
+  async getFileData(file, size) {
+    let { localPath } = file;
+    let data;
+
+    if (size) localPath = `${localPath}_${size}`;
+
+    try {
+      data = await fsPromises.readFile(localPath);
+    } catch (err) {
+      // console.log(err.message);
+      return { error: 'Not found', code: 404 };
+    }
+
+    return { data };
+  },
+};
+
+export default fileUtils;
